40d277a49c0b1cee5cc18661a25e2f0084ce25c6
Alex <as.lexus@gmail.com>
2018-10-30 11:22:04 +0200

    InfluxDBC-kafka.py 0.1

--- kafka/InfluxDBC-kafka.py
+++ kafka/InfluxDBC-kafka.py
@@ -6,26 +6,18 @@ import yaml
 import json
 
 # prepare Kafka producer
-try:
-	kafka = KafkaClient('blockchain-kafka-kafka.default.svc.cluster.local:9092')
-	producer = SimpleProducer(kafka)
-	print('kafka connection is OK')
-except Exception as ex:
-    print(ex)
+kafka = KafkaClient('blockchain-kafka-kafka.default.svc.cluster.local:9092')
+producer = SimpleProducer(kafka)
+print('kafka connection prepared')
 
-# Connect to InfluxDB
-try:
-	client = InfluxDBClient(host='influxdb-influxdb.default.svc.cluster.local', port=8086)
-	print('InfluxDB connection prepated')
-except Exception as ex:
-    print(ex)
+
+# prepare InfluxDB connection
+client = InfluxDBClient(host='influxdb-influxdb.default.svc.cluster.local', port=8086)
+print('InfluxDB connection prepated')
 	
 # Switch database
-try:
-	client.switch_database('prices')
-	print('InfluxDB database connected')
-except Exception as ex:
-    print(ex)
+client.switch_database('prices')
+print('InfluxDB database connected')
 	
 # get list of measurements
 measurements = client.get_list_measurements()
@@ -36,38 +28,21 @@ clear_USD = [x for x in measurements
               ]
 #clear measurements ending with '_BTC'
 clear_BTC = [x for x in clear_USD 
-               if x['name'].endswith('_BTC') == False
-              ]
+               if x['name'].endswith('_BTC') == False]
 
 #create test dataset
-test_dict = [x for x in measurements if x['name'] == '611_USD']			  
+test_dict = [x for x in measurements if x['name'] == 'ALC_allcoin']			  
 
 #try to read data from InfluxDB
-try:
-	for x in test_dict: #this is just a simple dataset for testing connection
-	#for x in clear_BTC: #use this to get all filtered measurements from InfluxDB
-		print('handling ' + x['name'])
-		results = client.query(('SELECT * from "%s" ORDER by time DESC LIMIT 1') %  x['name'])
-		points = list(results.get_points())
-		#uncomment the row below to send the query results to kafka
-		#producer.send_messages('test', json.dumps(points, indent=4, default=json_util.default).encode('utf-8'))
-		
-		#save results into a file
-		data_file = x['name']+'.json'
-		with open(data_file, 'w') as outfile:
-			json.dump(points, outfile)
-			print(data_file + ' saved')
-except Exception as ex:
-    print(ex)
 
-#try to send data to kafka
-try:
-	with open(data_file) as f:
-		for line in f:
-			d = yaml.safe_load(line)
-			jd = json.dumps(d)
-			#send message
-			producer.send_messages('test', json.dumps(jd, indent=4, default=json_util.default).encode('utf-8'))
-			print('message sent')
-except Exception as ex:
-	print(ex)
+for x in test_dict: #this is just a simple dataset for testing connection
+#for x in clear_BTC: #use this to get all filtered measurements from InfluxDB
+	print('handling ' + x['name'])
+	results = client.query(('SELECT * from "%s" WHERE time >= now() - 1d LIMIT 100') %  x['name'])
+	points = list(results.get_points())
+	for i, p in enumerate(points):
+		#add measure name
+		points[i]['name'] = x['name']
+		#send the query results to kafka
+		producer.send_messages('test', json.dumps(points[i], default=json_util.default).encode('utf-8'))
+	print(x['name'] + ' sent')	
